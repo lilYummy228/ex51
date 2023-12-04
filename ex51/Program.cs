96a1ef1b711b2bb1baa2178bdbb3572f49970923
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-12-04 12:23:12 +0500

    Добавьте файлы проекта.

---
+++ ex51/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex51
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class ServiceStation
+    {
+        private List<Detail> _details;
+
+    }
+
+    class Detail
+    {
+
+    }
+}
